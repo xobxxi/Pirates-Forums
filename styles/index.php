c38a6a04497638fd56ef65476743f98d56e4113a
Davy D <admin@piratesforums.com>
2011-01-15 15:36:17 -0800

    Updated statics to support multiple domains

--- styles/index.php
+++ styles/index.php
@@ -1,12 +1,19 @@
 <?php
 
 // config
-$static   = 'piratesoffline.org';
+$statics   = array(
+	'piratesoffline.org',
+	'mcraging.com'
+);
 $original = 'piratesforums.com';
 
-if ($_SERVER['HTTP_HOST'] == $static) {
-    header("HTTP/1.1 301 Moved Permanently");
-	header("Location: http://{$original}{$_SERVER['REQUEST_URI']}");
+foreach ($static as $static)
+{
+	if ($_SERVER['HTTP_HOST'] == $static) {
+	    header("HTTP/1.1 301 Moved Permanently");
+		header("Location: http://{$original}{$_SERVER['REQUEST_URI']}");
+		break;
+	}
 }
 
 echo 'Static resources';
