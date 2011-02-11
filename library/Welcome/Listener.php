e153df793095bca922c1525896837e6cbd0bb908
Davy D <admin@piratesforums.com>
2011-02-11 09:13:56 -0800

    Pirate Profile Directory links in sidebar now open in overlays

--- library/Welcome/Listener.php
+++ library/Welcome/Listener.php
@@ -4,7 +4,6 @@ class Welcome_Listener
 {
         public static function loadClassController($class, &$extend)
         {
-
 			if ($class == 'XenForo_ControllerPublic_Register') 
 			{
                 $extend[] = 'Welcome_ControllerPublic_Register';
