8e66ce53584cc6db471e28060a2f947a23a3904d
Davy D <admin@piratesforums.com>
2011-03-08 17:12:25 -0800

    New CommentsPlus [*] Currently enables editing of comments

---
+++ library/CommentsPlus/LikeHandler/ProfilePostComment.php
@@ -0,0 +1,27 @@
+<?php
+
+class LikeComments_LikeHandler_ProfilePostComment extends XenForo_LikeHandler_Abstract
+{
+	public function incrementLikeCounter($contentId, array $latestLikes, $adjustAmount = 1)
+	{
+		$dw = XenForo_DataWriter::create('XenForo_DataWriter_ProfilePostComment');
+		$dw->setExistingData($contentId);
+		$dw->set('likes', $dw->get('likes') + $adjustAmount);
+		$dw->set('like_users', $latestLikes);
+		$dw->save();
+	}
+
+	public function getContentData(array $contentIds, array $viewingUser)
+	{
+		$profilePostModel = XenForo_Model::create('XenForo_Model_ProfilePost');
+	
+		$comments = $profilePostModel->getProfilePostCommentsByIds($contentIds);
+		
+		return $comments; 
+	}
+
+	public function getListTemplateName()
+	{
+		return false;
+	}
+}
