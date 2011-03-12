36fdc18e21867327c3cc120318bdd08d8ea9ac64
Davy D <admin@piratesforums.com>
2011-03-12 11:10:59 -0800

    Fixing bug

--- library/PirateProfile/ViewPublic/Pirate/Find.php
+++ library/PirateProfile/ViewPublic/Pirate/Find.php
@@ -4,14 +4,19 @@ class PirateProfile_ViewPublic_Pirate_Find extends XenForo_ViewPublic_Base
 {
 	public function renderJson()
 	{
-		$results = array();
-		foreach ($this->_params['pirates'] AS $pirate)
+		if (is_array($this->_params['pirates']))
 		{
-			$results[$pirate['name']] = htmlspecialchars($pirate['name']);
-		}
+			$results = array();
+			foreach ($this->_params['pirates'] AS $pirate)
+			{
+				$results[$pirate['name']] = htmlspecialchars($pirate['name']);
+			}
 
-		return array(
-			'results' => $results
-		);
+			return array(
+				'results' => $results
+			);
+		}
+		
+		return false;
 	}
 }
