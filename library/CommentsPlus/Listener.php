2bcb76fcb37752b750d71b7471031fa5c02de07b
Davy D <admin@piratesforums.com>
2011-03-29 17:50:47 -0700

    Updating CommentsPlus [*] Fixed padding [*] Alert prefs

--- library/CommentsPlus/Listener.php
+++ library/CommentsPlus/Listener.php
@@ -34,4 +34,24 @@ class CommentsPlus_Listener
 				break;
 		}
 	}
+	
+	public static function templateCreate(&$templateName, array &$params, XenForo_Template_Abstract $template)
+	{
+		switch ($templateName)
+		{
+			case 'account_alert_preferences':
+				$template->preloadTemplate('commentsPlus_alert_preferences');
+				break;
+		}
+	}
+	
+	public static function templateHook($hookName, &$contents, array $hookParams, XenForo_Template_Abstract $template)
+	{
+		switch ($hookName)
+		{
+			case 'account_alerts_messages_on_profile_pages':
+				$contents .= $template->create('commentsPlus_alert_preferences', $hookParams)->render();
+				return $contents;
+		}
+	}
 }
