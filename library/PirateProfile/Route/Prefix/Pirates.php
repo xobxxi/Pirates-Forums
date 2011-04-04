d8ac152c584cb4563a204c322058896d4009019b
Davy D <admin@piratesforums.com>
2011-04-03 19:43:18 -0700

    Minor fixes to styling and phrases

--- library/PirateProfile/Route/Prefix/Pirates.php
+++ library/PirateProfile/Route/Prefix/Pirates.php
@@ -5,12 +5,14 @@ class PirateProfile_Route_Prefix_Pirates implements XenForo_Route_Interface
 	public function match($routePath, Zend_Controller_Request_Http $request, XenForo_Router $router)
 	{
 		$action = $router->resolveActionWithIntegerParam($routePath, $request, 'id');
+		
 		return $router->getRouteMatch('PirateProfile_ControllerPublic_Pirate', $action, 'pirates');
 	}
 
 	public function buildLink($originalPrefix, $outputPrefix, $action, $extension, $data, array &$extraParams)
 	{	
-		if (!empty($data['pirate_id'])) {
+		if (!empty($data['pirate_id']))
+		{
 			return XenForo_Link::buildBasicLinkWithIntegerParam($outputPrefix, $action, $extension, $data, 'pirate_id', 'name');
 		}
 
