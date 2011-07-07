efb87b59da6d885285c9bde97ec95b14b9579b95
Davy D <admin@piratesforums.com>
2011-07-06 19:20:56 -0700

    Updating FileSums
    I really need to start making this a habit!

--- library/ConversationEnhanced/FileSums.php
+++ library/ConversationEnhanced/FileSums.php
@@ -12,7 +12,7 @@ class ConversationEnhanced_FileSums
   'library/ConversationEnhanced/Install.php' => '8c24f47016675c8c05d3f2aac7c520d3',
   'library/ConversationEnhanced/Listener.php' => '25573cf9b530f6b1d80bd054a55e9a34',
   'library/ConversationEnhanced/Model/Conversation.php' => '85049fe434d3f431f0b7dce5cd57c25f',
-  'library/ConversationEnhanced/ReportHandler/ConversationMessage.php' => '720d5edbbd14c30405968c27f266d3aa',
+  'library/ConversationEnhanced/ReportHandler/ConversationMessage.php' => '64c806e5e0f03351a33cea9c9773c9ea',
   'library/ConversationEnhanced/ViewPublic/Conversation/View.php' => '6fcfe3d261e31a969747c561c2ee803c',
   'library/ConversationEnhanced/ViewPublic/Conversation/ViewMessage.php' => 'f89280c40c58e5581ca72a107f784091',
   'library/ConversationEnhanced/ViewPublic/Conversation/ViewNewMessages.php' => '200d157a44ec0f5e6d7abd41a51d334a',
