f0160dd6e405007ce8fd388445876b8d8ec159fa
Davy D <admin@piratesforums.com>
2011-07-08 17:36:38 -0700

    Disabling update (bug)

--- library/Album/DataWriter/User.php
+++ library/Album/DataWriter/User.php
@@ -2,12 +2,12 @@
 
 class Album_DataWriter_User extends XFCP_Album_DataWriter_User
 {
-    public static $usernameChangeUpdates += array
+    /*public static $usernameChangeUpdates += array
 	(
 		'permanent' => array
 		(
 			'album_photo_comment_username'
 				=> array('album_photo_comment', 'username', 'user_id')
 		)
-	);
+	);*/
 }
