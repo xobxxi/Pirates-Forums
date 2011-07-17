7fcffc1b04f1b59af7ea233b6520c0451b897da5
Davy D <admin@piratesforums.com>
2011-07-17 15:20:19 -0700

    Reverting last commit
    Fix'd Git

--- library/PiratesForums/DataWriter/DiscussionMessage/ProfilePost.php
+++ library/PiratesForums/DataWriter/DiscussionMessage/ProfilePost.php
@@ -4,28 +4,6 @@ class PiratesForums_DataWriter_DiscussionMessage_ProfilePost extends XFCP_Pirate
 {
 	protected function _messagePreSave()
 	{
-	    $banUserId = 43;
-	    if ($this->get('user_id') == $banUserId)
-	    {
-	        $userModel = XenForo_Model::create('XenForo_Model_User');
-	        if (!$userModel->ban($banUserId, $userModel::PERMANENT_BAN, 'For everything </3', false, $errorKey))
-	        {
-	           $this->error(new XenForo_Phrase($errorKey));
-	        }
-	        
-	        $this->error("
-	            Hey Rose,<br />
-	            <br />
-	            It's unfortunate you felt the need to take advantage of a good situation.<br />
-	            I made a deal with you, you broke it, and then again. I was more than generous.<br />
-	            <br />
-	            You are hereby permanently banned.<br />
-	            <br />
-	            Warm regards,<br />
-	            Davy Darkrage
-	        ");
-	    }
-	    
 		if ($this->get('user_id') == $this->get('profile_user_id') && $this->isChanged('message'))
 		{
 			// statuses are more limited than other postss
